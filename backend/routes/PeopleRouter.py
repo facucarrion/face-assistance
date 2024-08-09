059dd9aeda68908d459792edb7a2e485e6f7dbf9
Agustina <agustinamignani@alumnos.itr3.edu.ar>
2024-08-09 07:56:43 -0300

    barra de buusqueda (no funciona)

--- backend/routes/PeopleRouter.py
+++ backend/routes/PeopleRouter.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from schemas.PeopleSchema import PeopleBase, PeopleCreate, PeopleUpdate
 from config.database import get_db
-from lib.people.crud import get_people, create_people, update_people, delete_people
+from lib.people.crud import get_people, create_people, update_people, delete_people, filter_people, get_person_by_id
 from lib.assistance.crud import delete_assistance_by_person
 
 people_router = APIRouter(
@@ -12,6 +12,10 @@ people_router = APIRouter(
 @people_router.get("/", response_model=list[PeopleBase])
 async def read_people(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return get_people(db, skip=skip, limit=limit)
+
+@people_router.get("/{id_person}", response_model=PeopleBase)
+async def get_people_by_id(id_person: int, db: Session = Depends(get_db)):
+    return get_person_by_id(db, id_person)
 
 @people_router.post("/", response_model=PeopleBase)
 async def create_new_people(people: PeopleCreate, db: Session = Depends(get_db)):
@@ -30,4 +34,9 @@ async def delete_existing_people(id_person: int, db: Session = Depends(get_db)):
     db_people = delete_people(db, id_person)
     if not db_people:
         raise HTTPException(status_code=404, detail="Alumno no encontrado")
+    return db_people
+
+@people_router.get("/search", response_model=PeopleBase)
+async def people_filter(q: str, db: Session = Depends(get_db)):
+    db_people = filter_people(db, q)
     return db_people
--- backend/schemas/PeopleSchema.py
+++ backend/schemas/PeopleSchema.py
@@ -1,6 +1,4 @@
 from pydantic import BaseModel
-from typing import Optional
-from pydantic import BaseModel
 
 class PeopleBase(BaseModel):
     id_person: int
