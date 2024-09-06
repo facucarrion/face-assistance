0bc2515be4dbc48ada402f6791f4d4d7a6100c8b
Agustina <agustinamignani@alumnos.itr3.edu.ar>
2024-09-06 17:30:56 -0300

    prueba

--- frontend/src/components/ManageComponent.jsx
+++ frontend/src/components/ManageComponent.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import UserForm from './userForm.jsx'
 import GroupsForm from './groupsForm.jsx'
 import PeopleForm from './peopleForm.jsx'
+import ScheduleForm from './scheduleForm.jsx'
 
 const ManageComponent = () => {
   const [view, setView] = useState('users')
@@ -27,6 +28,12 @@ const ManageComponent = () => {
           className='bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
         >
           Gestionar Alumnos
+        </button>
+        <button
+          onClick={() => setView('schedules')}
+          className='bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          Gestionar Horarios
         </button>
       </div>
 
